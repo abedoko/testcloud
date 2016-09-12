e744c73143230986479bbad44fca6c6e5f1e3aab
ludai <372412490@qq.com>
2016-09-12 10:35:44 +0800

    建立配置文件
    添加环境

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,88 @@
+var path = require('path')
+var cssLoaders = require('./css-loaders')
+var projectRoot = path.resolve(__dirname, '../')
+
+module.exports = {
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist/static'),
+    publicPath: '/release/cloud/static/',
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.vue'],
+    fallback: [path.join(__dirname, '../node_modules')],
+    alias: {
+      'src': path.resolve(__dirname, '../src')
+    }
+  },
+  resolveLoader: {
+    fallback: [path.join(__dirname, '../node_modules')]
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.vue$/,
+        loader: 'eslint',
+        include: projectRoot,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        include: projectRoot,
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        include: projectRoot,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.html$/,
+        loader: 'vue-html'
+      },
+      {
+        test: /\.(png|jpg|gif|svg|woff2?|eot|ttf)(\?.*)?$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: '[name].[ext]?[hash:7]'
+        }
+      },
+      {
+        test: /favicon.ico$/,
+        loader: 'file',
+        query: {
+          name: '[name].[ext]'
+        }
+      },
+      {
+        test: /plupload\.full\.min\.js$/,
+        loader: 'imports?this=>window',
+      }
+    ]
+  },
+  vue: {
+    autoprefixer: {
+      browsers: ['last 2 version', 'ie 9']
+    },
+    loaders: cssLoaders()
+  },
+  eslint: {
+    formatter: require('eslint-friendly-formatter')
+  }
+}
